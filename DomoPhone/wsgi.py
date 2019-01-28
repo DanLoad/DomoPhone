d84dbcf15e076a6a9e45cdc1568ef5e5b935f359
DANsystem <danmetalist@gmail.com>
2019-01-28 20:29:08 +0600

    modules

--- DomoPhone/wsgi.py
+++ DomoPhone/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/DomoPhone')
+# adjust the Python version in the line below as needed
+#sys.path.append('/var/www/vhosts/mysite/venv/lib/python3.5/site-packages')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DomoPhone.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
