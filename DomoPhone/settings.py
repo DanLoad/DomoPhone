81644ec96557961738ad232810f689459b017545
DANsystem <danmetalist@gmail.com>
2019-01-30 15:45:05 +0600

    Django ok

--- DomoPhone/settings.py
+++ DomoPhone/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
