8a23d35db1d5233f2fa94a0a9213975add3cdb59
DANsystem <danmetalist@gmail.com>
2019-01-30 16:48:05 +0600

    rfid OK

--- modules/RFID/RFID.py
+++ modules/RFID/RFID.py
@@ -1,23 +1,25 @@
+﻿## -*- coding: utf-8 -*-
+
 import serial
 import time
 
-Tag1 = str('22009C45F40F')
+Tag1 = str('2200B8D3C28B')
 uart = serial.Serial("/dev/ttyS2", baudrate=9600, timeout=3000)
 
-print "OK"
+print ("OK")
 while True:
     ID = ""
     read_byte = uart.read()
-    if read_byte=="\x02":
+    if str(read_byte.decode('utf-8')) == '\x02':
         for Counter in range(12):
             read_byte=uart.read()
-            ID = ID + str(read_byte)
-            print hex(ord( read_byte))
-        print ID
+            ID = ID + str(read_byte.decode('utf-8'))
+            #print (read_byte)
+        print (ID)
         if ID == Tag1:
-            print "matched"
+            print ("matched")
             time.sleep(5)
         else:
-            print "Access Denied"
+            print ("Access Denied")
             time.sleep(5)
             uart.flushInput()
